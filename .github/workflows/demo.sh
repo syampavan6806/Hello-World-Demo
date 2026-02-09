25765778908bf54231f4864aff8d51f063377261
syampavan6806 <nagasyampawan6806@gmail.com>
2026-02-09 18:22:47 +0530

    updated demo.sh  file

---
+++ .github/workflows/demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a demo script for the Hello-World-Demo repository.
+date
+echo "Hello, World!"
+echo "This is a demo script for GitHub Actions."
+pwd
+ls -la
+df -h
+cat demo.sh
